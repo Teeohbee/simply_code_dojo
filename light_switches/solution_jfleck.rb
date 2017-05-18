fdc1dd3f1dd6401312a2de98dd55fa245e186895
Joshua Fleck <joshua.fleck@simplybusiness.co.uk>
2017-05-18 18:58:37 +0100

    Improve method naming

--- light_switches/solution_jfleck.rb
+++ light_switches/solution_jfleck.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-def range(s, e)
-  ((2**((e-s)+1) - 1) << s)
+def bitmap(startpoint, endpoint)
+  ((2**((endpoint-startpoint)+1) - 1) << startpoint)
 end
 
 size = nil
@@ -9,11 +9,11 @@ switches = 0b0
 print 'Please enter the number of switches> '
 while line=gets
   if size
-    points = line.split.sort.map(&:to_i)
-    s = points[0]
-    e = points[1]
-    range = range(s, e)
-    puts "current state is #{switches.to_s(2)}, range is #{points} or #{range.to_s(2)}, new state is #{(switches = switches ^ range).to_s(2)}, there are #{switches.to_s(2).count('1')} lights on"
+    points     = line.split.sort.map(&:to_i)
+    startpoint = points[0]
+    endpoint   = points[1]
+    bitmap     = bitmap(startpoint, endpoint)
+    puts "current state is #{switches.to_s(2)}, range is #{points} or #{bitmap.to_s(2)}, new state is #{(switches = switches ^ bitmap).to_s(2)}, there are #{switches.to_s(2).count('1')} lights on"
   else
     size = line.to_i
     puts "Size is #{size}"
